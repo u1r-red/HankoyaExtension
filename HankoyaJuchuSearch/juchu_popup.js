00241c36ca1021c404fede32a6672586590b4b5c
y-yamamoto <y-yamamoto@amida.co.jp>
2019-06-19 17:36:36 +0900

    kintone通知画面(iframe)に対応 一部jQuery可

--- HankoyaJuchuSearch/juchu_popup.js
+++ HankoyaJuchuSearch/juchu_popup.js
@@ -1,13 +1,11 @@
-/*
-window.alert('action in context');
-
-var requestData = {greeting: "hello"};
-chrome.runtime.sendMessage({greeting: "hello"});
-*/
-document.addEventListener('selectionchange', function() {
-    var selection = window.getSelection().toString().trim();
-    chrome.runtime.sendMessage({
-        request: 'updateContextMenu',
-        selection: selection
-    });
+console.debug('action in juchu_popup context');
+$(function(){
+	$(document).on('selectionchange', function() {
+	    var selection = window.getSelection().toString().trim();
+		console.log('selection chaned: ', selection);		
+	    chrome.runtime.sendMessage({
+	        request: 'updateContextMenu',
+	        selection: selection
+	    });
+	});
 });
