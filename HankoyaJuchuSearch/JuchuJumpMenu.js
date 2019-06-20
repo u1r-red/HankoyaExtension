e2e66831a01f5d524e9fb28743650678e2232e0c
y-yamamoto <y-yamamoto@amida.co.jp>
2019-06-20 11:16:34 +0900

    firefox/chrome両方でのポップアップ対応

--- HankoyaJuchuSearch/JuchuJumpMenu.js
+++ HankoyaJuchuSearch/JuchuJumpMenu.js
@@ -3,19 +3,35 @@
  */
 var JuchuJumpMenu = (function() {
 	
+	
 	var JuchuJumpMenu = function() {
 		this._urlResolver = new JuchuUrl();
 		
-		var url = this._urlResolver.urlToList();		
-		this._contextMenuID = chrome.contextMenus.create({
+		var url = this._urlResolver.urlToList();
+		
+		this._contextMenuID = "HankoyaJumpJuchu";
+		chrome.contextMenus.create({
+			  id : this._contextMenuID,
 			  title: 'kintone受注検索画面へ',
 			  contexts: ['selection'],
-			  type: 'normal',
-			  onclick: (info, tab) => {
-				window.open(url);
-			  }
+			  type: 'normal'
 			});
 
+		chrome.contextMenus.onClicked.addListener(
+			(info,tab) => {
+				console.debug('menu action:',info.menuItemId);
+				switch(info.menuItemId) {
+				case this._contextMenuID:
+					console.log("selected:",info.selectionText);
+					var url = this._urlResolver.urlToQuery(info.selectionText);
+					chrome.tabs.create({
+						active: true,
+						url:url
+					});
+					break;
+				}
+			}
+		);
 	}
 	
 	var p = JuchuJumpMenu.prototype;
@@ -24,20 +40,14 @@ var JuchuJumpMenu = (function() {
 		if( selected ) {
 			var url = this._urlResolver.urlToQuery(selected);
 		    chrome.contextMenus.update(this._contextMenuID, {
-		    	title: 'kintoneで受注ID['+selected+']を検索',
-				  onclick: (info, tab) => {
-					  window.open(url);
-				  }
+		    	title: 'kintoneで受注ID['+selected+']を検索'
 		    });
 
 		} else {
 			var url = this._urlResolver.urlToList();
 		    chrome.contextMenus.update(this._contextMenuID, {
-		    	title : 'kintone受注検索画面へ',
-				onclick: (info, tab) => {
-					window.open(url);
-				}
-		    });
+		    	title : 'kintone受注検索画面へ'
+			});
 		}
 	};
 	
